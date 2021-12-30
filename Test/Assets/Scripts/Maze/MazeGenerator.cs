5442c3a1b2cf1df7170359af3b866221eb5f6c95
Timely <timbelentsov@gmail.com>
2021-12-30 22:54:55 +0300

    add cell

--- Test/Assets/Scripts/Maze/MazeGenerator.cs
+++ Test/Assets/Scripts/Maze/MazeGenerator.cs
@@ -5,34 +5,34 @@ using UnityEngine;
 
 public class MazeGeneratorCell
 {
-    public int X { get; private set; }
-    public int Y { get; private set; }
-
     public MazeGeneratorCell(int x, int y)
     {
-        X = x;
-        Y = y;
+        X = x * 2;
+        Z = y * 2;
     }
+    public int X { get; private set; }
+    public int Z { get; private set; }
 
     public bool WallLeft = true;
     public bool WallBottom = true;
 }
 
-public class MazeGenerator 
+public class MazeGenerator : IEnumerable
 {
-    public int Width = 23;
-    public int Height = 15;
-
-    public MazeGeneratorCell[,] GenerateMaze()
+    private MazeGeneratorCell[,] maze;
+    public MazeGeneratorCell[,] GenerateMaze(int lenX, int lenZ)
     {
-        MazeGeneratorCell[,] maze = new MazeGeneratorCell[Width, Height];
+        maze = new MazeGeneratorCell[lenX, lenZ];
+
         for (int x = 0; x < maze.GetLength(0); x++)
-        {
-            for (int y = 0; y < maze.GetLength(1); y++)
-            {
-                maze[x, y] = new MazeGeneratorCell(x, y);
-            }
-        }
+            for (int z = 0; z < maze.GetLength(1); z++)
+                maze[x, z] = new MazeGeneratorCell(x, z);
+
         return maze;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return maze.GetEnumerator();
     }
 }
--- Test/Assets/Scripts/Maze/MazeSpawner.cs
+++ Test/Assets/Scripts/Maze/MazeSpawner.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class MazeSpawner : MonoBehaviour
 {
     public GameObject cellPrefab;
+    private float offsetX = -9.75f;
+    private float offsetZ = -4.75f;
 
     private void Start()
     {
         MazeGenerator generator = new MazeGenerator();
+        MazeGeneratorCell[,] maze = generator.GenerateMaze(10, 7);
+
+        foreach (var item in maze)
+        {
+            Instantiate(cellPrefab, new Vector3(item.X + offsetX, 0, item.Z + offsetZ),
+                                                                  Quaternion.identity);
+        }
     }
 }
